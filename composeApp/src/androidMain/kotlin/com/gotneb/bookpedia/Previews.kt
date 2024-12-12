19d6b1e19453ed719d9d7df445cc468400d53b14
Gabriel Bento <gabriel_holanda@hotmail.com.br>
2024-12-12 08:44:50 -0400

    feat: done book list
    Implemented:
    - The home screen UI (list of book)
    - Style of book in the list

--- composeApp/src/androidMain/kotlin/com/gotneb/bookpedia/Previews.kt
+++ composeApp/src/androidMain/kotlin/com/gotneb/bookpedia/Previews.kt
@@ -3,6 +3,10 @@ package com.gotneb.bookpedia
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.gotneb.bookpedia.book.domain.Book
+import com.gotneb.bookpedia.book.presentation.book_list.BookListScreen
+import com.gotneb.bookpedia.book.presentation.book_list.BookListState
+import com.gotneb.bookpedia.book.presentation.components.BookList
 import com.gotneb.bookpedia.book.presentation.components.BookSearchBar
 
 @Preview(showBackground = true)
@@ -15,4 +19,31 @@ fun Previews() {
             onSearchQueryChange = {},
         )
     }
+}
+
+private val books = (1..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imageUrl = "https://test.com",
+        authors = listOf("Gabriel Bento"),
+        description = "Description $it",
+        languages = emptyList(),
+        firstPublishYear = null,
+        averageRating = 4.57241,
+        ratingCount = 12,
+        numPages = 100,
+        numEditions = 3,
+    )
+}
+
+@Preview
+@Composable
+private fun BookListScreenPreview() {
+    BookListScreen(
+        state = BookListState(
+            searchResults = books
+        ),
+        onAction = {},
+    )
 }
--- composeApp/src/commonMain/kotlin/com/gotneb/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/gotneb/bookpedia/book/presentation/book_list/BookListState.kt
@@ -1,11 +1,13 @@
 package com.gotneb.bookpedia.book.presentation.book_list
 
 import com.gotneb.bookpedia.book.domain.Book
+import com.gotneb.bookpedia.core.presentation.UiText
 
 data class BookListState(
-    val searchQuery: String = "",
+    val searchQuery: String = "Kotlin",
     val searchResults: List<Book> = emptyList(),
     val favoritesBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
+    val errorMessage: UiText? = null
 )
